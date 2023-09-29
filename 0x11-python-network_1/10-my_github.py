1ec2af752eb260649dd43d119bb48c549c6d6685
tabby400 <tabbynargaret@gmail.com>
2023-09-29 10:32:59 -0700

    github API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" this involves using github api  to
+display your id"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
