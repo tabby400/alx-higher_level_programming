756f9a70ddbb04d122708328bc341b534d68dacb
tabby400 <tabbymargaret@gmail.com>
2023-11-01 08:06:52 -0700

    inputs

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+
+$('document').ready(function () {
+    $('div#add_item').click(function () {
+        $('ul.my_list').append('<li>Item</li>');
+    });
+    $('div#remove_item').click(function () {
+        $('ul.my_list li:last').remove();
+    });
+    $('div#clear_list').click(function () {
+        $('UL.my_list').empty();
+    });
+});
